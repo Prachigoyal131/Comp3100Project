ed23015537909e45e1243b310d853b4a6d3c81b3
Prachigoyal131 <87845901+Prachigoyal131@users.noreply.github.com>
2023-03-28 16:32:09 +1100

    Created MyClient java file
    Did three way handshake.

---
+++ MyClient.java
@@ -0,0 +1,54 @@
+import java.io.*;   
+import java.net.*;  //For socket programming
+
+public class MyClient{
+    public static void main(String args[]){
+
+        try{
+            Socket s = new Socket("localhost",50000); //using default Port no.
+            DataOutputStream dout = new DataOutputStream(s.getOutputStream());
+            BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+
+            String Server_R;
+            String Client_R;
+
+            Client_R = "HELO\n";
+            dout.write(Client_R.getBytes());
+            dout.flush();
+            Server_R = in.readLine();
+            System.out.println("Server says "+Server_R);
+
+            String User = System.getProperty("user.name");
+            Client_R = "AUTH " +User+ "\n";
+            dout.write(Client_R.getBytes());
+            dout.flush();
+            Server_R = in.readLine();
+            System.out.println("Server says" +Server_R);
+
+            Client_R = "REDY\n";
+            dout.write(Client_R.getBytes());
+            dout.flush();
+            Server_R = in.readLine();
+            System.out.println("Server says "+Server_R);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            dout.close();
+            s.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+       
+    }
+}
